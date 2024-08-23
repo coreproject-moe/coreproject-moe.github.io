4a7abbf338df1455f6f3478ed33551501221785e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-23 22:36:05 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { defineCustomElements } from "@coreproject-moe/icons/loader";
 	import { onMount } from "svelte";
-	import icons from "@coreproject-moe/icons-generator";
 
 	console.log(icons);
 	onMount(() => {
