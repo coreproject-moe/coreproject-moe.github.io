fdf54efb533e01f9dbef29e65306f6ab779dad50
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-23 22:36:51 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { defineCustomElements } from "@coreproject-moe/icons/loader";
-	import { onMount } from "svelte";
+    import { defineCustomElements } from '@coreproject-moe/icons/loader';
+    import { onMount } from 'svelte';
 
-	console.log(icons);
-	onMount(() => {
-		defineCustomElements(window);
-	});
+    onMount(() => {
+        defineCustomElements(window);
+    });
 </script>
 
 <slot />
