db23cff88284c01a32576b4125a80b5ef14a60a5
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-29 10:11:34 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true
-		},
-		chunkSizeWarningLimit: 2048,
-		emptyOutDir: true,
-		target: "esnext",
-		cssTarget: "esnext",
-		minify: "terser",
-		sourcemap: true
-	}
+    optimizeDeps: {
+        // exclude: ['@coreproject-moe/icons'],
+    },
+    plugins: [sveltekit()],
+    esbuild: {
+        target: 'esnext',
+        legalComments: 'external',
+    },
+    css: {
+        devSourcemap: true,
+        // Switch to lightning.css when tailwind supports it
+        transformer: 'postcss',
+    },
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        chunkSizeWarningLimit: 2048,
+        emptyOutDir: true,
+        target: 'esnext',
+        cssTarget: 'esnext',
+        minify: 'terser',
+        sourcemap: true,
+    },
+    worker: {
+        format: 'es',
+    },
 });
