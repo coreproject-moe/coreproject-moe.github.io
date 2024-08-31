f193f269e6cd8bc730305d63ed3c524af29924db
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-08-31 12:50:19 +0600

    Add trigram search (#9)
    * Add trigram search
    
    * add

--- src/lib/functions/icons.ts
+++ src/lib/functions/icons.ts
@@ -1,13 +1,15 @@
 export function given_icon_name_return_html_string({
-  icon_name,
-  classname,
-  variant,
+    icon_name,
+    classname,
+    icon_type,
+    variant,
 }: {
-  icon_name: string;
-  classname?: string;
-  variant?: string;
+    icon_name: string;
+    icon_type: string;
+    classname?: string;
+    variant?: string;
 }) {
-  if (variant)
-    return `<${icon_name} ${classname ? `class=${classname}` : ""} variant='${variant}'></${icon_name}>`;
-  return `<${icon_name}${classname ? ` class=${classname}` : ""}></${icon_name}>`;
+    if (variant)
+        return `<coreproject-${icon_type}-${icon_name} ${classname ? `class=${classname}` : ''} variant='${variant}'></coreproject-${icon_type}-${icon_name}>`;
+    return `<coreproject-${icon_type}-${icon_name}${classname ? ` class=${classname}` : ''}></coreproject-${icon_type}-${icon_name}>`;
 }
--- src/routes/icons/+layout.svelte
+++ src/routes/icons/+layout.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-  import { base } from "$app/paths";
+    import { base } from '$app/paths';
 </script>
 
 <div class="relative flex h-dvh w-full flex-col bg-secondary overflow-y-scroll">
-  <header
-    class="sticky top-0 z-10 bg-secondary/90 backdrop-blur flex items-center justify-between md:h-16 md:px-3 container"
-  >
-    <a href="{base}/icons">
-      <coreproject-shape-logo class="w-9 md:w-[2.25vw] md:pt-[0.75vw]">
-      </coreproject-shape-logo>
-    </a>
-    <nav class="flex items-center gap-5">
-      <a href="{base}/icons" class="text-accent">Icons</a>
-      <a href="{base}/icons/usage">Usage</a>
-      <a href="https://github.com/coreproject-moe/monorepo">
-        <coreproject-logo-github class="size-5"> </coreproject-logo-github>
-      </a>
-    </nav>
-  </header>
-  <div class="flex flex-1 justify-center">
-    <div class="max-w-xl">
-      <slot />
+    <header
+        class="sticky top-0 z-10 bg-secondary/90 backdrop-blur flex items-center justify-between md:h-16 md:px-3 container"
+    >
+        <a href="{base}/icons">
+            <coreproject-shape-logo class="w-9 md:w-[2.25vw] md:pt-[0.75vw]">
+            </coreproject-shape-logo>
+        </a>
+        <nav class="flex items-center gap-5">
+            <a href="{base}/icons" class="text-accent">Icons</a>
+            <!-- <a href="{base}/icons/usage">Usage</a> -->
+            <a href="https://github.com/coreproject-moe/monorepo">
+                <coreproject-logo-github class="size-5">
+                </coreproject-logo-github>
+            </a>
+        </nav>
+    </header>
+    <div class="flex flex-1 justify-center">
+        <div class="max-w-xl">
+            <slot />
+        </div>
     </div>
-  </div>
 </div>
