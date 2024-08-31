e2550e1e6839342a4456f4d544a8bd5096de4802
moonlit_grace <moonlitgrace.gaia@gmail.com>
2024-08-31 12:52:13 +0530

    Fix: icons dialog (#10)

--- src/lib/components/pages/icons/icon-dialog.svelte
+++ src/lib/components/pages/icons/icon-dialog.svelte
@@ -1,56 +1,66 @@
 <script lang="ts">
-  export let icon: string;
-  export let uuid: string;
-  export let variant: string | undefined = undefined;
+	const {
+		icon,
+		type,
+		uuid,
+		variant
+	}: {
+		icon: string;
+		type: string;
+		uuid: string;
+		variant?: string;
+	} = $props();
 
-  import { given_icon_name_return_html_string } from '$lib/functions/icons';
+	import { given_icon_name_return_html_string } from "$lib/functions/icons";
 </script>
 
 <dialog id={uuid} class="modal backdrop:bg-transparent">
-  <div
-    class="modal-box max-w-max p-0 absolute bottom-10 bg-secondary h-max"
-  >
-    <div class="bg-neutral/50 p-3 w-full flex items-center">
-      <div class="flex flex-col gap-2">
-        <div class="flex items-center gap-2">
-          <div class="size-5">
-            {@html given_icon_name_return_html_string({
-              icon_name: icon,
-              classname: "text-accent",
-              variant,
-            })}
-          </div>
-          <span class="text-accent font-semibold text-lg">
-            {icon}
-          </span>
-          <button
-            onclick={(e) => {
-            const target = e.target as HTMLButtonElement;
-              navigator.clipboard
-                .writeText(
-                  given_icon_name_return_html_string({
-                    icon_name: icon,
-                    classname: "text-accent",
-                  }),
-                )
-                .then(() => {
-                  target.innerText = "Copied";
-                  setTimeout(() => target.innerText = "Copy", 2000);
-                });
-            }}
-            class="ml-auto btn btn-xs btn-primary">Copy</button
-          >
-        </div>
-        <code class="text-sm bg-secondary/75 rounded-xl p-2">
-          {given_icon_name_return_html_string({
-            icon_name: icon,
-            classname: "text-accent",
-          })}
-        </code>
-      </div>
-    </div>
-  </div>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
+	<div class="modal-box absolute bottom-10 h-max max-w-max bg-secondary p-0">
+		<div class="flex w-full items-center bg-neutral/50 p-3">
+			<div class="flex flex-col gap-2">
+				<div class="flex items-center gap-2">
+					<div class="size-5">
+						{@html given_icon_name_return_html_string({
+							icon_type: type,
+							icon_name: icon,
+							classname: "text-accent",
+							variant
+						})}
+					</div>
+					<span class="text-lg font-semibold text-accent">
+						{icon}
+					</span>
+					<button
+						onclick={(e) => {
+							const target = e.target as HTMLButtonElement;
+							navigator.clipboard
+								.writeText(
+									given_icon_name_return_html_string({
+										icon_type: type,
+										icon_name: icon,
+										classname: "text-accent"
+									})
+								)
+								.then(() => {
+									target.innerText = "Copied";
+									setTimeout(() => (target.innerText = "Copy"), 2000);
+								});
+						}}
+						class="btn btn-primary btn-xs ml-auto">Copy</button
+					>
+				</div>
+				<code class="rounded-xl bg-secondary/75 p-2 text-sm">
+					{given_icon_name_return_html_string({
+						icon_type: type,
+						icon_name: icon,
+						classname: "text-accent",
+						variant
+					})}
+				</code>
+			</div>
+		</div>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
 </dialog>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,59 +1,59 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import '../global.css';
-    import { defineCustomElements } from '@coreproject-moe/icons/loader';
+	import { onMount } from "svelte";
+	import "../global.css";
+	import { defineCustomElements } from "@coreproject-moe/icons/loader";
 
-    onMount(() => {
-        defineCustomElements(globalThis.window);
-    });
+	onMount(() => {
+		defineCustomElements(globalThis.window);
+	});
 </script>
 
 <div class="bg-secondary">
-    <slot></slot>
+	<slot></slot>
 </div>
 
 <!-- svelte-ignore css_unused_selector -->
 <style lang="scss" global>
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-SemiBoldItalic.woff2') format('woff2');
-        font-weight: 600;
-        font-style: italic;
-        font-display: swap;
-    }
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-SemiBold.woff2') format('woff2');
-        font-weight: 600;
-        font-style: normal;
-        font-display: swap;
-    }
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-BoldItalic.woff2') format('woff2');
-        font-weight: bold;
-        font-style: italic;
-        font-display: swap;
-    }
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-Italic.woff2') format('woff2');
-        font-weight: normal;
-        font-style: italic;
-        font-display: swap;
-    }
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-Regular.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-        font-display: swap;
-    }
-    @font-face {
-        font-family: 'Kokoro';
-        src: url('../fonts/Kokoro/Kokoro-Bold.woff2') format('woff2');
-        font-weight: bold;
-        font-style: normal;
-        font-display: swap;
-    }
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-SemiBoldItalic.woff2") format("woff2");
+		font-weight: 600;
+		font-style: italic;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-SemiBold.woff2") format("woff2");
+		font-weight: 600;
+		font-style: normal;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-BoldItalic.woff2") format("woff2");
+		font-weight: bold;
+		font-style: italic;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-Italic.woff2") format("woff2");
+		font-weight: normal;
+		font-style: italic;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-Regular.woff2") format("woff2");
+		font-weight: normal;
+		font-style: normal;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: "Kokoro";
+		src: url("../fonts/Kokoro/Kokoro-Bold.woff2") format("woff2");
+		font-weight: bold;
+		font-style: normal;
+		font-display: swap;
+	}
 </style>
