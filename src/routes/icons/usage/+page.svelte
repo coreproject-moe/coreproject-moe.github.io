8b796fa163762408a0b76d0a0bcb75683f9bfb2c
moonlit_grace <moonlitgrace.gaia@gmail.com>
2024-09-01 12:08:57 +0530

    Feat: work on usage page (#16)
    Co-authored-by: baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>

---
+++ src/routes/icons/usage/+page.svelte
@@ -0,0 +1,136 @@
+<script lang="ts">
+	import Highlight from "$lib/components/highlight.svelte";
+
+	const installation_mapping: { type: string; code: String; language: string }[] = [
+		{
+			type: "React (Vite)",
+			code: `
+// src/App.tsx
+import { defineCustomElements } from "@coreproject-moe/icons/loader";
+
+function App(){
+	defineCustomElements();
+	return <></>
+}
+		`,
+			language: "tsx"
+		},
+		{
+			type: "React (Next.js)",
+			code: `
+// app/layout.tsx
+import { defineCustomElements } from "@coreproject-moe/icons/loader";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  defineCustomElements();
+
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+  	`,
+			language: "tsx"
+		},
+		{
+			type: "SvelteKit",
+			code: `
+<!-- src/routes/layout.svelte -->
+<script lang='ts'>
+	import { defineCustomElements } from "@coreproject-moe/icons/loader";
+	import { onMount } from 'svelte';
+
+	onMount(()=>{
+		defineCustomElements();
+	})
+<\/script>
+		`,
+			language: "Html"
+		},
+		{
+			type: "Html",
+			code: `
+<script type="module" src='https://unpkg.com/@coreproject-moe/icons@latest'><\/script>
+			`,
+			language: "html"
+		}
+	];
+</script>
+
+<div
+	class="prose prose-invert px-5 py-10 prose-headings:mb-2 prose-p:my-2 prose-pre:my-4 prose-pre:rounded-xl prose-pre:bg-neutral/50 md:px-0 md:py-20"
+>
+	<h1 class="text-warning">Usage</h1>
+	<p class="!mt-8 text-lg text-info">
+		Coreicons is a completely open-source icon set crafted for web, iOS, Android, and desktop apps.
+		Coreicons was made for CoreProject, a streaming platform.
+	</p>
+	<h2>Installation</h2>
+	<p>
+		You can install Coreicons from npm with your favorite package manager. And place the following
+		code in your main entry file.
+	</p>
+	<Highlight language="bash" code="npm install @coreproject-moe/icons" />
+
+	Here is how to use it with different frameworks:
+	<div role="tablist" class="tabs tabs-lifted">
+		{#each installation_mapping as item, idx}
+			<input
+				checked={idx === 0}
+				type="radio"
+				name="code_items"
+				role="tab"
+				class="tab !w-max bg-transparent !outline-none !ring-0 checked:!bg-neutral/50 checked:!bg-[url('')]"
+				aria-label={item.type}
+			/>
+			<div
+				role="tabpanel"
+				class={`tab-content overflow-x-scroll [&>pre]:m-0 ${idx === 0 && "[&>pre]:rounded-tl-none"}`}
+			>
+				<Highlight language={item.language} code={item.code.trim()} />
+			</div>
+		{/each}
+	</div>
+	<h2>Basic Usage</h2>
+	<p>
+		To use a built-in icon from the Ionicons package, populate the <code>name</code> attribute on
+		the <code>coreproject-[shape|logo]-[icon-name]</code>
+		component:
+	</p>
+	<Highlight language="html" code="<coreproject-shape-airplay></coreproject-shape-airplay>" />
+	<h2>Variants</h2>
+	<p>
+		There are some icons with variants, you can use different variants of one icon with an <code
+			>attr</code
+		>: <code>variant</code> like:
+	</p>
+	<Highlight
+		language="html"
+		code="<coreproject-shape-align variant='justify'></coreproject-shape-align>"
+	/>
+	<h2>Properties / Attributes</h2>
+	<p>
+		You can provide
+		{#each ["width", "height", "_style"] as item}
+			<code>{item}</code>
+		{/each}
+		that will be passed to the shadow svg. Here is an example:
+	</p>
+	<Highlight
+		language="html"
+		code="<coreproject-shape-airplay class='text-primary' width='200' height='200' _style='color: black'></coreproject-shape-airplay>"
+	/>
+	<hr />
+	<h3>Issue Tracker</h3>
+	<p>
+		If you see any issue with icons, feel free to raise an issue <a
+			href="https://github.com/coreproject-moe/monorepo/issues">here.</a
+		>
+	</p>
+	<p>Thank you!</p>
+</div>
