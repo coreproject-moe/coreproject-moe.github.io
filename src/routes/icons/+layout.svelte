9bc6fc1a431d1b826c450c5ba6c40e56025a3efc
moonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-07 22:02:42 +0530

    refactor: fix: header navs

--- src/routes/icons/+layout.svelte
+++ src/routes/icons/+layout.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
-	import { base } from "$app/paths";
 	import Coreicons from "$lib/components/icons/coreicons.svelte";
+	import { page } from "$app/stores";
 
 	let { children } = $props();
 </script>
 
 <div class="relative h-dvh w-full overflow-y-scroll bg-secondary">
 	<header
-		class="container sticky top-0 z-10 flex items-center justify-between bg-secondary/90 px-3 py-2 backdrop-blur md:h-16 md:px-3 md:py-0"
+		class="container sticky top-0 z-10 flex items-center justify-between bg-secondary/90 px-3 py-4 backdrop-blur md:h-16 md:px-3 md:py-0"
 	>
 		<div class="flex items-center gap-2">
-			<a href="{base}/icons">
+			<a href="/icons">
 				<coreproject-shape-logo class="flex size-7"> </coreproject-shape-logo>
 			</a>
 			<Coreicons class="hidden h-5 md:flex" />
 		</div>
 		<nav class="flex items-center gap-4 md:gap-5">
-			<a href="{base}/icons" class="text-accent">Icons</a>
-			<a href="{base}/icons/usage">Usage</a>
+			{#each Object.entries({ icons: "/icons", usage: "/icons/usage" }) as item}
+				<a
+					href={item[1]}
+					class="capitalize transition-colors"
+					class:text-accent={$page.url.pathname === item[1]}>{item[0]}</a
+				>
+			{/each}
 			<a href="https://github.com/coreproject-moe/monorepo">
-				<coreproject-logo-github class="size-5"> </coreproject-logo-github>
+				<coreproject-logo-github class="flex size-5"> </coreproject-logo-github>
 			</a>
 		</nav>
 	</header>
