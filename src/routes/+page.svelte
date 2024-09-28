5d549cd8ad348c171f5ac6381fcf414507b4400e
Noonlitgrace <moonlitgrace.gaia@gmail.com>
2024-09-28 08:54:05 +0530

    fix: responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,14 @@
 </script>
 
 <main class="grid h-screen w-screen place-items-center">
-	<section class="mx-auto flex w-full max-w-5xl items-center justify-between gap-20">
+	<section
+		class="mx-auto flex w-full flex-col flex-col-reverse items-center justify-between gap-20 p-5 md:max-w-5xl md:flex-row"
+	>
 		<div class="flex h-full flex-col justify-between gap-20">
-			<coreproject-shape-logo class="size-14"></coreproject-shape-logo>
+			<coreproject-shape-logo class="hidden size-14 md:flex"></coreproject-shape-logo>
 			<div class="flex flex-col items-start gap-4">
-				<Coreproject class="h-16 w-auto" />
-				<h2 class="text-2xl font-semibold">
+				<Coreproject class="h-12 w-auto md:h-16" />
+				<h2 class="text-xl font-semibold md:text-2xl">
 					Imagine a new platform where creativity thrives and limitless possibilities emerge.
 				</h2>
 				<div class="flex items-center gap-4">
@@ -30,6 +32,6 @@
 				<p>Join us in shaping the future of anime, manga, and sound streaming.</p>
 			</div>
 		</div>
-		<CoreIsometric class="flex h-[70vh] w-auto flex-shrink-0 overflow-visible" />
+		<CoreIsometric class="flex h-72 w-auto flex-shrink-0 overflow-visible md:h-[70vh]" />
 	</section>
 </main>
