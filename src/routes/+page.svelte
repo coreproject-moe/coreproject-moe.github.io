38b0eb4ceb508ece030c23722a1405a69fc32b1f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-02 20:36:51 +0530

    feat: github api integration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
 	import Header from "$lib/components/header.svelte";
 	import Vectors from "$lib/components/pages/home/vectors.svelte";
+	import { onMount } from "svelte";
+	import { fly } from "svelte/transition";
+
+	let contributers_count = $state(0);
+	let stargazers_count = $state(0);
+
+	onMount(async () => {
+		try {
+			const contributers_res = await fetch(
+				"https://api.github.com/repos/coreproject-moe/CoreProject/contributors?per_page=1&anon=true"
+			);
+			// github returns a link header for paginated results
+			const link_header = contributers_res.headers.get("Link");
+
+			if (link_header) {
+				// extract the last page number from the link header
+				const last_page_match = link_header.match(/page=(\d+)>; rel="last"/);
+				if (last_page_match) contributers_count = parseInt(last_page_match[1], 10);
+			}
+
+			// fetch stargazers count
+			const stargazers_res = await fetch(
+				"https://api.github.com/repos/coreproject-moe/CoreProject"
+			);
+			const stagazers_data = await stargazers_res.json();
+			stargazers_count = stagazers_data.stargazers_count;
+		} catch (err) {
+			console.error(err);
+		}
+	});
 </script>
 
 <svelte:head>
@@ -26,6 +56,25 @@
 	</div>
 	<!-- vectors -->
 	<Vectors />
+	<!-- API informations -->
+	{#if contributers_count}
+		<div in:fly={{ y: 5 }} class="info-box contributers">
+			<span class="head">contributers</span>
+			<div>
+				<span class="count">{contributers_count}</span>
+				<span class="badge">peeps</span>
+			</div>
+		</div>
+	{/if}
+	{#if stargazers_count}
+		<div in:fly={{ y: 5 }} class="info-box stargazers">
+			<span class="head">stargazers</span>
+			<div>
+				<span class="count">{stargazers_count}</span>
+				<span class="badge">stars</span>
+			</div>
+		</div>
+	{/if}
 </div>
 
 <style>
@@ -64,6 +113,61 @@
 				display: flex;
 				align-items: center;
 				gap: 0.75rem;
+			}
+		}
+
+		.info-box {
+			position: absolute;
+			padding: 1rem;
+			background-color: color-mix(in srgb, var(--color-neutral) 25%, transparent);
+			border: 1px solid var(--color-neutral);
+			backdrop-filter: blur(1rem);
+
+			&.contributers {
+				top: -4rem;
+				right: 15rem;
+
+				div span.badge {
+					background-color: color-mix(in srgb, var(--color-error) 25%, transparent);
+					color: var(--color-warning);
+				}
+			}
+
+			&.stargazers {
+				top: -2rem;
+				left: 10rem;
+
+				div span.badge {
+					background-color: color-mix(in srgb, var(--color-primary) 25%, transparent);
+					color: var(--color-accent);
+				}
+			}
+
+			span.head {
+				text-transform: uppercase;
+				font-weight: 600;
+				font-size: 0.75rem;
+				color: var(--color-info);
+				opacity: 75%;
+			}
+
+			div {
+				display: flex;
+				align-items: center;
+				gap: 0.5rem;
+
+				span.count {
+					font-weight: bold;
+					font-size: 1.75rem;
+				}
+
+				span.badge {
+					line-height: 0;
+					padding: 0.65rem 0.5rem;
+					border-radius: 1rem;
+					text-transform: uppercase;
+					font-size: 0.75rem;
+				}
 			}
 		}
 	}
