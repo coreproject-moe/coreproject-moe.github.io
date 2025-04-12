d9c9233a4bdfc79fd5abaaf900a6be97f74b0f7f
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-04-12 08:33:14 +0530

    revert: grids vector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -131,6 +131,64 @@
 				</div>
 			{/if}
 		</div>
+		<!-- grids -->
+		<svg
+			class="hero__vector hero__vector--grids"
+			width="86"
+			height="62"
+			viewBox="0 0 86 62"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<circle cx="1" cy="1" r="1" fill="#1E2036" />
+			<circle cx="13" cy="1" r="1" fill="#1E2036" />
+			<circle cx="25" cy="1" r="1" fill="#1E2036" />
+			<circle cx="37" cy="1" r="1" fill="#1E2036" />
+			<circle cx="49" cy="1" r="1" fill="#1E2036" />
+			<circle cx="61" cy="1" r="1" fill="#1E2036" />
+			<circle cx="73" cy="1" r="1" fill="#1E2036" />
+			<circle cx="85" cy="1" r="1" fill="#1E2036" />
+			<circle cx="1" cy="13" r="1" fill="#1E2036" />
+			<circle cx="13" cy="13" r="1" fill="#1E2036" />
+			<circle cx="25" cy="13" r="1" fill="#1E2036" />
+			<circle cx="37" cy="13" r="1" fill="#1E2036" />
+			<circle cx="49" cy="13" r="1" fill="#1E2036" />
+			<circle cx="61" cy="13" r="1" fill="#1E2036" />
+			<circle cx="73" cy="13" r="1" fill="#1E2036" />
+			<circle cx="85" cy="13" r="1" fill="#1E2036" />
+			<circle cx="1" cy="25" r="1" fill="#1E2036" />
+			<circle cx="13" cy="25" r="1" fill="#1E2036" />
+			<circle cx="25" cy="25" r="1" fill="#1E2036" />
+			<circle cx="37" cy="25" r="1" fill="#1E2036" />
+			<circle cx="49" cy="25" r="1" fill="#1E2036" />
+			<circle cx="61" cy="25" r="1" fill="#1E2036" />
+			<circle cx="73" cy="25" r="1" fill="#1E2036" />
+			<circle cx="85" cy="25" r="1" fill="#1E2036" />
+			<circle cx="1" cy="37" r="1" fill="#1E2036" />
+			<circle cx="13" cy="37" r="1" fill="#1E2036" />
+			<circle cx="25" cy="37" r="1" fill="#1E2036" />
+			<circle cx="37" cy="37" r="1" fill="#1E2036" />
+			<circle cx="49" cy="37" r="1" fill="#1E2036" />
+			<circle cx="61" cy="37" r="1" fill="#1E2036" />
+			<circle cx="73" cy="37" r="1" fill="#1E2036" />
+			<circle cx="85" cy="37" r="1" fill="#1E2036" />
+			<circle cx="1" cy="49" r="1" fill="#1E2036" />
+			<circle cx="13" cy="49" r="1" fill="#1E2036" />
+			<circle cx="25" cy="49" r="1" fill="#1E2036" />
+			<circle cx="37" cy="49" r="1" fill="#1E2036" />
+			<circle cx="49" cy="49" r="1" fill="#1E2036" />
+			<circle cx="61" cy="49" r="1" fill="#1E2036" />
+			<circle cx="73" cy="49" r="1" fill="#1E2036" />
+			<circle cx="85" cy="49" r="1" fill="#1E2036" />
+			<circle cx="1" cy="61" r="1" fill="#1E2036" />
+			<circle cx="13" cy="61" r="1" fill="#1E2036" />
+			<circle cx="25" cy="61" r="1" fill="#1E2036" />
+			<circle cx="37" cy="61" r="1" fill="#1E2036" />
+			<circle cx="49" cy="61" r="1" fill="#1E2036" />
+			<circle cx="61" cy="61" r="1" fill="#1E2036" />
+			<circle cx="73" cy="61" r="1" fill="#1E2036" />
+			<circle cx="85" cy="61" r="1" fill="#1E2036" />
+		</svg>
 	</div>
 </main>
 
@@ -190,6 +248,7 @@
 		&__vector {
 			z-index: -1;
 			position: absolute;
+			height: auto;
 
 			svg {
 				width: 100%;
@@ -237,6 +296,28 @@
 
 				@media (width >= 80rem) {
 					left: 0;
+				}
+			}
+
+			&--grids {
+				top: -2rem;
+				left: 2rem;
+				width: 10rem;
+
+				@media (width >= 40rem) {
+					left: 10rem;
+				}
+
+				@media (width >= 48rem) {
+					left: 15rem;
+				}
+
+				@media (width >= 64rem) {
+					left: 25rem;
+				}
+
+				@media (width >= 80rem) {
+					left: 25rem;
 				}
 			}
 		}
